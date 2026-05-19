86f3a875ceaa18cc9f56b97e286fc052135e4aaf
zhiyi <huangjiaqi041015@gmail.com>
2026-05-19 15:39:08 +0800

    add article-menu

--- src/components/ArticleDetails.tsx
+++ src/components/ArticleDetails.tsx
@@ -4,6 +4,12 @@ import ArticleEditorView from "@/components/ArticleEditorView.tsx";
 import Loading from "@/components/Loading.tsx";
 import { Route as ArticleRoute } from "@/routes/_app/article.$articleId.tsx";
 import { getArticleById } from "@/services/apiArticle.ts";
+import {
+  BookmarksIcon,
+  NotePencilIcon,
+  ThumbsUpIcon,
+  TrashIcon,
+} from "@phosphor-icons/react";
 
 function ArticleDetails() {
   const editor = useCreateBlockNote();
@@ -25,6 +31,34 @@ function ArticleDetails() {
       <h1 className="text-center text-7xl mb-8 mt-8 font-serif">
         {article?.title}
       </h1>
+
+      <div className="divider"></div>
+      <div className="flex justify-center">
+        <ul className="menu menu-horizontal  rounded-box mt-6">
+          <li>
+            <a className="tooltip" data-tip="Like">
+              <ThumbsUpIcon size={24} />
+            </a>
+          </li>
+          <li>
+            <a className="tooltip" data-tip="Bookmark">
+              <BookmarksIcon size={24} />
+            </a>
+          </li>
+          <li>
+            <a className="tooltip" data-tip="Edit">
+              <NotePencilIcon size={24} />
+            </a>
+          </li>
+          <li>
+            <a className="tooltip" data-tip="Delete">
+              <TrashIcon size={24} />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="divider"></div>
+
       <ArticleEditorView editor={editor} editable={false} />
     </>
   );
