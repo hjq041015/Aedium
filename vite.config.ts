e11a0ac74199289853839e8bb98742bfdefa1adf
zhiyi <huangjiaqi041015@gmail.com>
2026-05-21 14:01:53 +0800

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -20,5 +21,29 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset(), "jotai-babel/preset"] }),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Aedium',
+        short_name: 'Aedium',
+        description: 'A medium clone',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    })
   ],
 });
